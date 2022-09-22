53efe4d90709137ab497fff55f6dc315cb87a370
Polgár Kristóf <polgarkristof10@gmail.com>
2022-09-22 08:17:51 +0200

    Initial commit

---
+++ src/hu/petrik/aukcioproject/Festmeny.java
@@ -0,0 +1,5 @@
+package hu.petrik.aukcioproject;
+
+public class Festmeny {
+
+}
---
+++ src/hu/petrik/aukcioproject/main.java
@@ -0,0 +1,7 @@
+package hu.petrik.aukcioproject;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
